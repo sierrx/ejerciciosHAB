5fb415cfd7705d3be7c7590121d02a9acf736af6
sierrx <angelportador45@gmail.com>
2024-12-09 17:45:00 -0600

    creacion de las carpetas lol

---
+++ 01-Java-Basico/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package holamundo;
+
+public class HolaMundo {
+
+
+    public static void main(String[] args) {
+       
+       System.out.println("hola mundo");
+       //System.out.println("ola");
+        
+    }
+    
+}
