d3ddfb9c497aaf7ce630699f54c865a60cf38908
sierrx <angelportador45@gmail.com>
2024-12-09 18:49:49 -0600

    act

---
+++ 01-Java-Basico/TiposDeDatos/src/tiposdedatos/TiposDeDatos.java
@@ -0,0 +1,19 @@
+package tiposdedatos;
+
+public class TiposDeDatos {
+
+
+    public static void main(String[] args) {
+      //tipo de dato + nombre de la variable
+      int edad = 23;
+      double peso = 83.7;
+      boolean var = true;
+      String  nombre = "Angel";
+      
+      System.out.println("tu edad es: " + edad + "tu peso es de : " + peso + "y tu nombre es :" + nombre + "  " + var);
+        
+      
+      
+    }
+    
+}
